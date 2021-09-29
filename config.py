3630b2094d6ab690e2339e20b1b330d3b628168f
Chiranjeev_Khurana <chiranjeevkhurana11@gmail.com>
2021-09-29 14:40:11 +0530

    changed config

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 class Config:
 
     SECRET_KEY = "HEY"
 
-    GOOGLE_APPLICATION_CREDENTIALS="/home/chiranjeev/Documents/My_Codes/trusty-pixel-326015-8d16a9c16e0e.json"
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") or "/home/chiranjeev/Documents/My_Codes/trusty-pixel-326015-8d16a9c16e0e.json"
 
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://chiranjeev:chints11#@localhost:3306/aicte'
 

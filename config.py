bafb290c579b353d65b72bdc709415367b77646e
Chiranjeev_Khurana <chiranjeevkhurana11@gmail.com>
2021-09-29 13:54:59 +0530

    updated requirements.txt

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 class Config:
-    SECRE_KEY = "HEY"
+
+    SECRET_KEY = "HEY"
 
     GOOGLE_APPLICATION_CREDENTIALS="/home/chiranjeev/Documents/My_Codes/trusty-pixel-326015-8d16a9c16e0e.json"
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://chiranjeev:chints11#@localhost:3306/aicte'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
